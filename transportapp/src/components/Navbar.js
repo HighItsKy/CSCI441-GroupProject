b196290748ee69f4a3a93e1c8602241dc09aff32
Alex Hampton <ahampton5158@gmail.com>
2023-03-16 08:46:37 -0400

    Add data to navbar and loop through it

--- transportapp/src/components/Navbar.js
+++ transportapp/src/components/Navbar.js
@@ -1,13 +1,22 @@
 
 // in the function the return will show what the html will look like
 function Navbar() {
+    let isAdmin = true;
+
+    let adminLinks = ["View Jobs", "Add Job", "Edit Job", "Manage Users", "Account Settings"]
+    let driverLinks = ["View Jobs", "Edit Job", "Account Settings"]
+
+    // use adminLinks if isAdmin is true, use driverLinks otherwise
+    let linksToUse = isAdmin ? adminLinks : driverLinks
+
     return (
         // made sure there is only one html tag at the top level of the return statement
         <nav>
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Create Job</li>
+                {/* loop through our array and create a <li> element for each item */}
+                {linksToUse.map((link) => (
+                    <li>{link}</li>
+                ))}
             </ul>
         </nav>
 
