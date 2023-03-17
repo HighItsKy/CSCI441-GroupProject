c9b153b90b764a56c3c3a52239bff3d8d2ea62d1
Ashraf Alkhawaldeh <79298738+AshrafAlkhawaldeh@users.noreply.github.com>
2023-03-17 16:47:31 -0400

    Update Transport_app_PostgreSQL.sql

--- SQL/Transport_app_PostgreSQL.sql
+++ SQL/Transport_app_PostgreSQL.sql
@@ -36,7 +36,7 @@ CREATE TABLE Employee (
     Employee_City VARCHAR(255),
     Employee_State VARCHAR(13),
     Employee_Zip_Code VARCHAR(9),
-	Employee_Email VARCHAR (255)
+    Employee_Email VARCHAR (255)
 );
 
 
@@ -50,10 +50,10 @@ CREATE TABLE Job (
     Driver_signature BYTEA,
     Shipper_Signature BYTEA,
     Receiver_Signature BYTEA,
-	Date_of_Order DATE NOT NULL,
-	Date_Complete DATE,
+    Date_of_Order DATE NOT NULL,
+    Date_Complete DATE,
     Job_Status VARCHAR(255) 
-	CHECK (Job_Status IN ('Pending','Loading','Enroute','Arrived','Unloaded','Complete')),
+    CHECK (Job_Status IN ('Pending','Loading','Enroute','Arrived','Unloaded','Complete')),
     Special_Instructions VARCHAR (2000),
     FOREIGN KEY (Shipper_ID) REFERENCES Customer(Customer_ID),
     FOREIGN KEY (Receiver_ID) REFERENCES Customer(Customer_ID),
@@ -78,28 +78,28 @@ CREATE TABLE Photo_Record (
     Photo_ID SERIAL PRIMARY KEY NOT NULL,
     Car_Line_Item_ID INTEGER NOT NULL,
     URL VARCHAR(4096) NOT NULL,
-	Note VARCHAR (2000),
+    Note VARCHAR (2000),
     FOREIGN KEY (Car_Line_Item_ID) REFERENCES Car_Line_Item(Car_Line_Item_ID)
 );
 
 /*To save companies details*/
 
 CREATE TABLE Company (
-	Company_ID SERIAL PRIMARY KEY NOT NULL,
-	Company_Name VARCHAR(255) UNIQUE NOT NULL
+     Company_ID SERIAL PRIMARY KEY NOT NULL,
+     Company_Name VARCHAR(255) UNIQUE NOT NULL
 );
 
 /*To save company branches in case we are transferring from one branch to another for the same company*/
 
 CREATE TABLE Company_Branch (
-	Company_Branch_ID SERIAL PRIMARY KEY NOT NULL,
-	Company_ID INTEGER NOT NULL,
-	Branch_Name VARCHAR(255) UNIQUE NOT NULL,
-	Branch_Contact_No VARCHAR(20) NOT NULL,
+    Company_Branch_ID SERIAL PRIMARY KEY NOT NULL,
+    Company_ID INTEGER NOT NULL,
+    Branch_Name VARCHAR(255) UNIQUE NOT NULL,
+    Branch_Contact_No VARCHAR(20) NOT NULL,
     Branch_Street_Address VARCHAR(255),
     Branch_City VARCHAR(255),
     Branch_State VARCHAR(13),
     Branch_Zip_Code VARCHAR(9),
-	Branch_Email VARCHAR (255),
-	FOREIGN KEY (Company_ID) REFERENCES Company(Company_ID)
+    Branch_Email VARCHAR (255),
+    FOREIGN KEY (Company_ID) REFERENCES Company(Company_ID)
 );
