f08c827f6f3869df4884ef9043719af62005bd33
Alex Hampton <ahampton5158@gmail.com>
2023-03-19 14:21:58 -0400

    update routes

--- transportapp/src/App.js
+++ transportapp/src/App.js
@@ -1,33 +1,27 @@
 import './App.css';
-import { Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, redirect, Switch } from 'react-router-dom'
 // import any components you want to use in this file:
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About'; 
 import Login from './components/Login';
-// use Route, Router instead
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
 // use state instead of variables
 let isUserLoggedIn = true;
 let currentPath = isUserLoggedIn ? <Home /> : <Login />
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: currentPath
-  }
-])
-
 
 
 function App() {
   return (
-    <div className='App'>
-      <RouterProvider router={router} />
-    </div>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ currentPath }></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
