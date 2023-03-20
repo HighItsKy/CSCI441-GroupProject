f89b5051aad4e7cd1fdd84a5950bba768d011a66
HighItsKy <73318996+HighItsKy@users.noreply.github.com>
2023-03-19 22:52:58 -0500

    Added code for header

--- transportapp/src/components/header.js
+++ transportapp/src/components/header.js
@@ -1,1 +1,8 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<link href="https://fonts.cdnfonts.com/css/eczar?styles=19319" rel="stylesheet">
+<link href="index.css" rel="stylesheet">
+    <div class="headerImage"></div>	
+	<div class="headerText">
+		Oklahoma Trucks Direct Transportation
+	</div>
