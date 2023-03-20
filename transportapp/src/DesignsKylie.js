85795960ab76b1782aaf8482a1397f6459cd3487
HighItsKy <73318996+HighItsKy@users.noreply.github.com>
2023-03-19 22:31:21 -0500

    Full code of the index.html file

---
+++ transportapp/src/DesignsKylie.js
@@ -0,0 +1,29 @@
+/* 
+<!DOCTYPE html>
+<html lang="en">
+<link href="https://fonts.cdnfonts.com/css/eczar?styles=19319" rel="stylesheet">
+<link href="https://fonts.cdnfonts.com/css/london-tube" rel="stylesheet">
+	<link href="index.css" rel="stylesheet">
+<head>
+	<TITLE>Oklahoma Trucks Direct Transportation</TITLE>	
+</head>
+<body>
+	<div class="headerImage"></div>	
+	<div class="headerText">
+		Oklahoma Trucks Direct Transportation
+	</div>
+		<nav>
+		<ul>
+		<div class="centerNav">
+			<li><a href="index.html">Home</a></li>
+		</div>
+		<div class="rightNav">
+			<li><a href="login.html">Login</a></li>
+		</div>
+		</ul>
+		</nav>
+		<h2>Welcome to Oklahoma Trucks Direct Transportation!</h2>
+		<h1>To get started, please <a href="login.html" target="_blank">log in</a></h1>	
+</body>
+</HTML>
+*/
