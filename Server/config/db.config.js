4a650bfa6170035ad3d895f220641e28af154b05
Ashraf Alkhawaldeh <79298738+AshrafAlkhawaldeh@users.noreply.github.com>
2023-03-26 23:55:01 -0400

    Update db.config.js

--- Server/config/db.config.js
+++ Server/config/db.config.js
@@ -17,3 +17,6 @@ External Database URL : postgres://transport_app_9zwr_user:pOavrpCPXO30gh3T8eHaR
 
 PSQL Command : PPGPASSWORD=pOavrpCPXO30gh3T8eHaRYThhWyGaNnC psql -h dpg-cgchbjkeoogljtomqfh0-a.ohio-postgres.render.com -U transport_app_9zwr_user transport_app_9zwr
 */
+
+/* To access the Datebase from "connect URL" through Postbird use this URL "postgres://transport_app_9zwr_user:pOavrpCPXO30gh3T8eHaRYThhWyGaNnC@dpg-cgchbjkeoogljtomqfh0-a.ohio-postgres.render.com/transport_app_9zwr?ssl=true"
+
