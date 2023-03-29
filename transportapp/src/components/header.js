003f3f4c98aa674877cf9a0d81553a39efb91e44
HighItsKy <73318996+HighItsKy@users.noreply.github.com>
2023-03-29 17:44:46 -0500

    Added Header

--- transportapp/src/components/header.js
+++ transportapp/src/components/header.js
@@ -1,8 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<link href="https://fonts.cdnfonts.com/css/eczar?styles=19319" rel="stylesheet">
-<link href="index.css" rel="stylesheet">
-    <div class="headerImage"></div>	
-	<div class="headerText">
-		Oklahoma Trucks Direct Transportation
-	</div>
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+  
+// in the function the return will show what the html will look like
+function TransportHeader() {
+    return (
+        // made sure there is only one html tag at the top level of the return statement
+        <>		
+		<div className="bg-image">
+			<div className='d-flex justify-content-center align-items-center'>
+				<img src='images/transporterImage.png' className='img-fluid headerImage' alt='Car Transporter' />
+                		<span class="overlay-text">Oklahoma Trucks Direct Transportation</span>
+      			</div>
+    		</div>	
+        </>
+
+        // if i add another html tag here, then there would be two html tags at the top
+        // level and we would get an error
+        //<div Another html tag at the top level </div>
+    );
+}
+
+// export the component so it can be used outside of this file
+export default TransportHeader;
