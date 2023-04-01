92543308aab6ea543121f57a16e2dbad8d99dc1b
gbertman <gbertman@bertmanlaw.com>
2023-03-31 22:53:42 -0500

    fixing sync issues

--- Server/server.js
+++ Server/server.js
@@ -9,32 +9,6 @@ const errorHandler = require("./middleware/errorHandler");
 const exp = require("constants");
 const PORT = process.env.PORT || 3500;
 
-
-async function databaselookup() {
-
-    //use this for INSERT, UPDATE and DELETE - this supports transactions - YOU CAN DO MUTIPLE QUERIES IN ONE CONNECTION
-    try {
-        let client = await db.connect();
-
-        let data1 = await client.query('SELECT * FROM truck');
-        console.log(data1.rows[0]);
-        client.release();
-    }
-    catch (err) {
-        console.log(err.error);
-    }
-
-    //USE THIS FOR SELECTS - THIS USES ONE CONNECTION THEN RELEASES IT
-    try {
-        const data3 = await db.query('SELECT * FROM TRUCKs');
-        console.log(data3.rows[0])
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-databaselookup();
-
 // custom middleware logger
 app.use(logger);
 
@@ -53,6 +27,7 @@ app.use("/", express.static(path.join(__dirname, "/public")));
 
 // routes
 app.use("/", require("./routes/root"));
+app.use("/truck", require("./routes/api/truck"));
 //app.use("/TransportApp/TransportData", require("./routes/api/transportjobs"));
 //app.use("TransportApp", express.static(path.join(__dirname, "/views")));
 
