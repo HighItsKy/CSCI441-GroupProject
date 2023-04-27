792e9782bbb8179bda156c51e2dff4ea2cebc636
HighItsKy <73318996+HighItsKy@users.noreply.github.com>
2023-04-27 16:05:13 -0500

    Removed About from App.js,

--- transportapp/src/App.js
+++ transportapp/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route, Routes, redirect, Switch } from 'react-router-dom'
 // import any components you want to use in this file:
-import About from './components/About';
 import Login from './components/Login';
 import JobViewer from './components/Jobs/JobViewer';
 import TruckViewer from './components/Truck/TruckViewer';
 import ManageUsers from './components/Employees/ManageUsers';
 import AccountSettings from './components/User/AccountSettings';
-
 
 
 // // use state instead of variables
@@ -22,11 +20,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<JobViewer />}></Route>
                 <Route path="login" element={<Login />}></Route>
-                <Route path="about" element={<About />}></Route>
-                <Route path="JobViewer" element={<JobViewer />}></Route>
                 <Route path="TruckViewer" element={<TruckViewer />}></Route>
                 <Route path="ManageUsers" element={<ManageUsers />}></Route>
                 <Route path="AccountSettings" element={<AccountSettings />}></Route>
+                <Route path="JobViewer" element={<JobViewer />}></Route>
             </Routes>
         </BrowserRouter>
     );
