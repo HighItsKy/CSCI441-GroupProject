16eaeaf5d46d41b05b0db75aa7e354cd12462088
gbertman <gbertman@bertmanlaw.com>
2023-05-04 14:10:09 -0500

    updated user form with onchange

--- transportapp/src/components/Employees/UserForm.js
+++ transportapp/src/components/Employees/UserForm.js
@@ -6,55 +6,66 @@ import Form from 'react-bootstrap/Form';
 function UserForm({ user, setUser }) {
     const [errMsg, setErrMsg] = useState("");
 
+    const changeVal = async (key, value) => {
+        setUser(user => ({ ...user, [key]: value }));
+    }
+
     /*Determines whether the user being created is an admin*/
     let [isAdmin, setAdmin] = useState("true");
     /*Determines whether the user being created is a driver*/
     let [isDriver, setDriver] = useState("false");
 
-    const handleForm = async () => {
-            if(!user.employee_id){ //If the employee ID is blank, a new user is being added.
-                try {
-                    const newUser = { is_admin: isAdmin, is_driver: isDriver, employee_first_name: user.employee_first_name, employee_last_name: user.employee_last_name, 
-                        employee_username: user.employee_username, employee_contact_no: user.employee_contact_no,
-                         employee_street_address: user.employee_street_address, 
-                         employee_city: user.employee_city, employee_state: user.employee_state, employee_zip_code: user.employee_zip_code,
-                         employee_email: user.employee_email};
-                    let data = JSON.stringify(newUser);
-                    const response = await axios.post('/employee', data, {
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
+    const handleForm = async (e) => {
+
+        e.preventDefault();
+
+        if (!user.employee_id) { //If the employee ID is blank, a new user is being added.
+            try {
+                const newUser = {
+                    is_admin: isAdmin, is_driver: isDriver, employee_first_name: user.employee_first_name, employee_last_name: user.employee_last_name,
+                    employee_username: user.employee_username, employee_contact_no: user.employee_contact_no,
+                    employee_street_address: user.employee_street_address,
+                    employee_city: user.employee_city, employee_state: user.employee_state, employee_zip_code: user.employee_zip_code,
+                    employee_email: user.employee_email
+                };
+                let data = JSON.stringify(newUser);
+                const response = await axios.post('/employee', data, {
+                    headers: {
+                        'Content-Type': 'application/json'
                     }
-                    );
-                    //fetchItem();
-    
-                } catch (err) {
-                    setErrMsg(JSON.stringify(err));
-                    return;
                 }
-            } else {
-                try {
-                    const newUser = { is_admin: isAdmin, is_driver: isDriver, employee_first_name: user.employee_first_name, employee_last_name: user.employee_last_name, 
-                        employee_username: user.employee_username, employee_contact_no: user.employee_contact_no,
-                         employee_street_address: user.employee_street_address, 
-                         employee_city: user.employee_city, employee_state: user.employee_state, employee_zip_code: user.employee_zip_code,
-                         employee_email: user.employee_email};
-                    let data = JSON.stringify(newUser);
-                    const response = await axios.put('/employee', data, {
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    }
-                    );
-                    //fetchItem();
-    
-                } catch (err) {
-                    setErrMsg(JSON.stringify(err));
-                    return;
-                }
+                );
+                //fetchItem();
+
+            } catch (err) {
+                setErrMsg(JSON.stringify(err));
+                return;
             }
-            
-        
+        } else {
+            try {
+                const newUser = {
+                    is_admin: isAdmin, is_driver: isDriver, employee_first_name: user.employee_first_name, employee_last_name: user.employee_last_name,
+                    employee_username: user.employee_username, employee_contact_no: user.employee_contact_no,
+                    employee_street_address: user.employee_street_address,
+                    employee_city: user.employee_city, employee_state: user.employee_state, employee_zip_code: user.employee_zip_code,
+                    employee_email: user.employee_email
+                };
+                let data = JSON.stringify(newUser);
+                const response = await axios.put('/employee', data, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                );
+                //fetchItem();
+
+            } catch (err) {
+                setErrMsg(JSON.stringify(err));
+                return;
+            }
+        }
+
+
 
     }
 
@@ -68,48 +79,46 @@ function UserForm({ user, setUser }) {
                 <form className='employee-details' data-transport-order="form">
                     <div className='form-group'>
                         <label htmlFor='UserFName'>* FIRST NAME</label>
-                        <input type="text" className="form-control" id="UserFName" required value={user.employee_first_name}></input>
+                        <input type="text" className="form-control" id="employee_first_name" required value={user.employee_first_name} onChange={(e) => changeVal(e.currentTarget.id, e.currentTarget.value)}></input>
                         <label htmlFor='UserLName'>* LAST NAME</label>
-                        <input className="form-control" id="UserLName" required value={user.employee_last_name}></input>
+                        <input className="form-control" id="employee_last_name" required value={user.employee_last_name} onChange={(e) => changeVal(e.currentTarget.id, e.currentTarget.value)}></input>
                         <label htmlFor='Username'>* USERNAME</label>
-                        <input className="form-control" id="Username" required value={user.employee_username}></input>
+                        <input className="form-control" id="employee_username" required value={user.employee_username} onChange={(e) => changeVal(e.currentTarget.id, e.currentTarget.value)}></input>
                         <label htmlFor='UserPassword'>PASSWORD</label>
                         <input className="form-control" id="UserPassword" readOnly></input>
                         <label htmlFor='UserContactNo'>* CONTACT #</label>
-                        <input className="form-control" id="UserContactNo" required value={user.employee_contact_no}></input>
+                        <input className="form-control" id="employee_contact_no" required value={user.employee_contact_no} onChange={(e) => changeVal(e.currentTarget.id, e.currentTarget.value)}></input>
                         <label htmlFor='UserAddress'> STREET ADDRESS</label>
-                        <input className="form-control" id="UserAddress" value={user.employee_street_address}></input>
+                        <input className="form-control" id="employee_street_address" value={user.employee_street_address} onChange={(e) => changeVal(e.currentTarget.id, e.currentTarget.value)}></input>
                         <label htmlFor='UserCity'> CITY</label>
-                        <input className="form-control" id="UserCity"  value={user.employee_city}></input>
+                        <input className="form-control" id="employee_city" value={user.employee_city} onChange={(e) => changeVal(e.currentTarget.id, e.currentTarget.value)}></input>
                         <label htmlFor='UserState'>STATE</label>
-                        <input className="form-control" id="UserState"  value={user.employee_state}></input>
+                        <input className="form-control" id="employee_state" value={user.employee_state} onChange={(e) => changeVal(e.currentTarget.id, e.currentTarget.value)}></input>
                         <label htmlFor='UserZip'>ZIP CODE</label>
-                        <input className="form-control" id="UserZip"  value={user.employee_zip_code}></input>
+                        <input className="form-control" id="employee_zip_code" value={user.employee_zip_code} onChange={(e) => changeVal(e.currentTarget.id, e.currentTarget.value)}></input>
                         <label htmlFor='UserEmail'>EMAIL</label>
-                        <input type="email" placeholder="example@gmail/yahoo.com" pattern=".+@(gmail|yahoo)\.com" className="form-control" id="UserEmail" value={user.employee_email}></input>
+                        <input type="email" placeholder="example@gmail/yahoo.com" pattern=".+@(gmail|yahoo)\.com" className="form-control" id="employee_email" value={user.employee_email} onChange={(e) => changeVal(e.currentTarget.id, e.currentTarget.value)}></input>
                     </div>
                     <Form>
-                    {
-                        <div key={`default-checkbox`} className="mb-3" >
-                            <Form.Check 
-                                type={'checkbox'}
-                                onChange={() => {
-                                    if (isAdmin === "false" && isDriver === "true"){ //If the checkbox is already checked, user is Admin. 
-                                        setDriver("false");
-                                        setAdmin("true");
+                        {
+                            <div key={`default-checkbox`} className="mb-3" >
+                                <Form.Check
+                                    type={'checkbox'}
+                                    onChange={() => {
+                                        if (isAdmin === "false" && isDriver === "true") { //If the checkbox is already checked, user is Admin. 
+
+                                        }
+                                        else { //If the checkbox is already checked, user is Driver.
+                                        }
                                     }
-                                    else { //If the checkbox is already checked, user is Driver.
-                                        setDriver("true");
-                                        setAdmin("false");
-                                    }}
-                                } 
-                                id={`default-checkbox`}
-                                label={`Is Driver`}   
-                            />                    
-                        </div>
-                    }
+                                    }
+                                    id={`default-checkbox`}
+                                    label={`Is Driver`}
+                                />
+                            </div>
+                        }
                     </Form>
-                    <button type="save" className={"btn btn-primary"} onClick={handleForm}>SAVE</button>
+                    <button type="save" className={"btn btn-primary"} onClick={(e) => handleForm(e)}>SAVE</button>
                     <button type="reset" className={"btn btn-primary"}>RESET</button>
                     <button type="deleteUser" className={"btn btn-primary"}>DELETE USER</button>
                 </form>
