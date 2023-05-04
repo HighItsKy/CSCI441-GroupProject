3656027e4f0f0490f9b54a5515fc3d61128906d9
HighItsKy <73318996+HighItsKy@users.noreply.github.com>
2023-05-04 13:27:09 -0500

    Checkbox for driver

--- transportapp/src/components/Employees/UserForm.js
+++ transportapp/src/components/Employees/UserForm.js
@@ -1,29 +1,59 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from '../../api/axios';
+import Form from 'react-bootstrap/Form';
 
 function UserForm({ user, setUser }) {
     const [errMsg, setErrMsg] = useState("");
-    const handleForm = async () => {
-            try {
-                const saveUser = { employee_first_name: user.employee_first_name, employee_last_name: user.employee_last_name, 
-                    employee_username: user.employee_username, employee_contact_no: user.employee_contact_no,
-                     employee_street_address: user.employee_street_address, 
-                     employee_city: user.employee_city, employee_state: user.employee_state, employee_zip_code: user.employee_zip_code,
-                     employee_email: user.employee_email};
-                let data = JSON.stringify(saveUser);
-                const response = await axios.put('/employee', data, {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-                );
-                //fetchItem();
 
-            } catch (err) {
-                setErrMsg(JSON.stringify(err));
-                return;
+    /*Determines whether the user being created is an admin*/
+    let [isAdmin, setAdmin] = useState("true");
+    /*Determines whether the user being created is a driver*/
+    let [isDriver, setDriver] = useState("false");
+
+    const handleForm = async () => {
+            if(!user.employee_id){ //If the employee ID is blank, a new user is being added.
+                try {
+                    const newUser = { is_admin: isAdmin, is_driver: isDriver, employee_first_name: user.employee_first_name, employee_last_name: user.employee_last_name, 
+                        employee_username: user.employee_username, employee_contact_no: user.employee_contact_no,
+                         employee_street_address: user.employee_street_address, 
+                         employee_city: user.employee_city, employee_state: user.employee_state, employee_zip_code: user.employee_zip_code,
+                         employee_email: user.employee_email};
+                    let data = JSON.stringify(newUser);
+                    const response = await axios.post('/employee', data, {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    }
+                    );
+                    //fetchItem();
+    
+                } catch (err) {
+                    setErrMsg(JSON.stringify(err));
+                    return;
+                }
+            } else {
+                try {
+                    const newUser = { is_admin: isAdmin, is_driver: isDriver, employee_first_name: user.employee_first_name, employee_last_name: user.employee_last_name, 
+                        employee_username: user.employee_username, employee_contact_no: user.employee_contact_no,
+                         employee_street_address: user.employee_street_address, 
+                         employee_city: user.employee_city, employee_state: user.employee_state, employee_zip_code: user.employee_zip_code,
+                         employee_email: user.employee_email};
+                    let data = JSON.stringify(newUser);
+                    const response = await axios.put('/employee', data, {
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    }
+                    );
+                    //fetchItem();
+    
+                } catch (err) {
+                    setErrMsg(JSON.stringify(err));
+                    return;
+                }
             }
+            
         
 
     }
@@ -47,17 +77,38 @@ function UserForm({ user, setUser }) {
                         <input className="form-control" id="UserPassword" readOnly></input>
                         <label htmlFor='UserContactNo'>* CONTACT #</label>
                         <input className="form-control" id="UserContactNo" required value={user.employee_contact_no}></input>
-                        <label htmlFor='UserAddress'>* STREET ADDRESS</label>
-                        <input className="form-control" id="UserAddress" required value={user.employee_street_address}></input>
-                        <label htmlFor='UserCity'>* CITY</label>
-                        <input className="form-control" id="UserCity" required value={user.employee_city}></input>
-                        <label htmlFor='UserState'>* STATE</label>
-                        <input className="form-control" id="UserState" required value={user.employee_state}></input>
-                        <label htmlFor='UserZip'>* ZIP CODE</label>
-                        <input className="form-control" id="UserZip" required value={user.employee_zip_code}></input>
-                        <label htmlFor='UserEmail'>* EMAIL</label>
-                        <input type="email" placeholder="example@gmail/yahoo.com" pattern=".+@(gmail|yahoo)\.com" className="form-control" id="UserEmail" required value={user.employee_email}></input>
+                        <label htmlFor='UserAddress'> STREET ADDRESS</label>
+                        <input className="form-control" id="UserAddress" value={user.employee_street_address}></input>
+                        <label htmlFor='UserCity'> CITY</label>
+                        <input className="form-control" id="UserCity"  value={user.employee_city}></input>
+                        <label htmlFor='UserState'>STATE</label>
+                        <input className="form-control" id="UserState"  value={user.employee_state}></input>
+                        <label htmlFor='UserZip'>ZIP CODE</label>
+                        <input className="form-control" id="UserZip"  value={user.employee_zip_code}></input>
+                        <label htmlFor='UserEmail'>EMAIL</label>
+                        <input type="email" placeholder="example@gmail/yahoo.com" pattern=".+@(gmail|yahoo)\.com" className="form-control" id="UserEmail" value={user.employee_email}></input>
                     </div>
+                    <Form>
+                    {
+                        <div key={`default-checkbox`} className="mb-3" >
+                            <Form.Check 
+                                type={'checkbox'}
+                                onChange={() => {
+                                    if (isAdmin === "false" && isDriver === "true"){ //If the checkbox is already checked, user is Admin. 
+                                        setDriver("false");
+                                        setAdmin("true");
+                                    }
+                                    else { //If the checkbox is already checked, user is Driver.
+                                        setDriver("true");
+                                        setAdmin("false");
+                                    }}
+                                } 
+                                id={`default-checkbox`}
+                                label={`Is Driver`}   
+                            />                    
+                        </div>
+                    }
+                    </Form>
                     <button type="save" className={"btn btn-primary"} onClick={handleForm}>SAVE</button>
                     <button type="reset" className={"btn btn-primary"}>RESET</button>
                     <button type="deleteUser" className={"btn btn-primary"}>DELETE USER</button>
